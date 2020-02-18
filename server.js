9abe803359515d709f0df5392358e03e5f4280b2
Steven Miyauchi <swm9220@gmail.com>
2020-02-18 10:36:38 -0800

    app.listen function

--- server.js
+++ server.js
@@ -8,15 +8,21 @@ require('dotenv').config();
 const cors = require('cors');
 app.use(cors());
 
-app.get('/location', (req, res) => {
-  let city = req.query.city;
-  let location = new City;
+app.get('/', (request, response) => {
 
-});
+}) 
 
-function City(){
+// app.get('/location', (req, res) => {
+//   let city = req.query.city;
+//   let location = new City;
 
-}
+// });
+
+// function City(){
+
+// }
 
 const PORT = process.env.PORT || 3001;
-console.log(`listening on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
