baf80711f7f5d5c79ac729848c387d9a0e15edb7
Steven Miyauchi <swm9220@gmail.com>
2020-02-18 13:28:29 -0800

    weather working

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 'use strict';
-
 const express = require('express');
 const app = express();
-
 require('dotenv').config();
 
 const cors = require('cors');
@@ -10,17 +8,36 @@ app.use(cors());
 
 app.get('/location', (request, response) => {
   let city = request.query.city;
-  let geoData = require('/data/geo.json');
-
+  let geoData = require('./data/geo.json');
   let location = new City(city, geoData[0]);
   response.send(location);
 });
 
 function City(city, obj){
-  this.search_querty = city;
+  this.search_query = city;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
+}
+
+////////////////////////////////////WEATHER////////////////////////////////
+let weatherArr = [];
+
+app.get('/weather', (request, response) => {
+  let newWeather = request.query.data;
+  let weatherData = require('./data/darksky.json');
+  for (let i = 0; i < weatherData.daily.data.length; i++){
+    let weather = new Weather(weatherData, i);
+    weatherArr.push(weather);
+  }
+  response.send(weatherArr);
+});
+console.log(weatherArr);
+
+function Weather(obj, index){
+  this.summary = obj.daily.data[index].summary;
+  let date = new Date(obj.daily.data[index].time);
+  this.time = date.toDateString();
 }
 
 const PORT = process.env.PORT || 3001;
