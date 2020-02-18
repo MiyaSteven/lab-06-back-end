4d09f0711e48c48cea99f098bd3b1b51be9e12f9
carrington <carringtonbeard@live.com>
2020-02-18 13:27:50 -0800

    we got weather

--- server.js
+++ server.js
@@ -17,13 +17,40 @@ app.get('/location', (request, response) => {
 });
 
 function City(city, obj){
-  this.search_querty = city;
+  this.search_query = city;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
+}
+
+////////////////////////////////////WEATHER////////////////////////////////
+  let weatherArr = [];
+
+app.get('/weather', (request, response) => {
+  try{
+  let newWeather = request.query.data;
+  let weatherData = require('./data/darksky.json');
+
+  for(let i =0; i < weatherData.daily.data.length; i++){
+  let weather = new Weather(weatherData, i);
+  weatherArr.push(weather);
+  }
+  response.send(weatherArr);
+  }
+  catch(err){
+    console.log(err)
+  }
+});
+  console.log(weatherArr)
+
+function Weather(obj, index){
+  let date = new Date(obj.daily.data[index].time)
+  this.forecast = obj.daily.data[index].summary;
+  this.time = date.toDateString();
 }
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
+
