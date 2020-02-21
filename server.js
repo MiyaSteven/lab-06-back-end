ddc4260c19c7e1a85d3d8706a1936917a13d3bec
carrington <carringtonbeard@live.com>
2020-02-21 13:05:31 -0800

    we got movies

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/location', (request, response) => {
       .then(results => {
         let geoData = results.body;
         let location = new City(city, geoData[0]);
-        let insertSql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)';
+        let insertSql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);';
         let insertSafeValue = [location.search_query, location.formatted_query, location.latitude, location.longitude];
         client.query(insertSql, insertSafeValue);
         response.status(200).send(location);
@@ -113,4 +113,31 @@ function Hiking(obj){
   let splitDateTime = obj.conditionDate.split(' ')
   this.condition_date = splitDateTime[0];
   this.condition_time = splitDateTime[1];
+};
+
+/////////////////////////////////////// MOVIES ////////////////////////////////////////////////
+
+app.get('/movies', (request, response)=> {
+  let location = request.query.search_query;
+  let {latitude, longitude} = request.query;
+  console.log(request.query, 'from get movies');
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${location}&page=1&include_adult=true`;
+    superagent.get(url)
+      .then(results =>{
+        console.log(results.body, 'this is probably fight club');
+        let movieResults = results.body.results.map((obj) => new Movies(obj));
+        console.log('inside results function', movieResults);
+        response.send(movieResults)
+        console.log(response.body);
+      })
+});
+
+function Movies(obj){
+    this.title = obj.original_title;
+    this.overview = obj.overview;
+    this.average_votes = obj.vote_average;
+    this.total_votes = obj.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${obj.poster_path}`;
+    this.popular = obj.popularity;
+    this.released_on = obj.release_date;
 };
